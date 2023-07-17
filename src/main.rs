2ef90007cce11b82bd360205f85036d19529ad88
phucle <ltphuc@fossil.com>
2023-07-17 14:42:27 +0700

    Runable project

---
+++ src/main.rs
@@ -0,0 +1,89 @@
+#![no_std]
+#![no_main]
+
+use defmt::println;
+use defmt_rtt as _;
+use panic_halt as _;
+
+use cortex_m_rt::entry;
+
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
+
+#[entry]
+fn main() -> ! {
+    if let Some(board) = Board::take() {
+        let mut timer = Timer::new(board.TIMER0);
+        let mut display = Display::new(board.display_pins);
+
+        #[allow(non_snake_case)]
+        let letter_I = [
+            [0, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 0],
+        ];
+
+        let heart = [
+            [0, 1, 0, 1, 0],
+            [1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_R = [
+            [0, 1, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_u = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 1, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_s = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 0],
+            [0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_t = [
+            [0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+        ];
+        loop {
+            display.show(&mut timer, letter_I, 1000);
+            display.show(&mut timer, heart, 1000);
+            display.show(&mut timer, letter_R, 1000);
+            display.show(&mut timer, letter_u, 1000);
+            display.show(&mut timer, letter_s, 1000);
+            display.show(&mut timer, letter_t, 1000);
+            display.clear();
+            timer.delay_ms(250_u32);
+
+            println!("Display changed");
+        }
+    }
+
+    panic!("End");
+}
